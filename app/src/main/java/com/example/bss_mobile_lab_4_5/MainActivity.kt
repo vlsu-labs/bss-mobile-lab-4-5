049e498e1ae22d6b8d257e09d68c56abd33f9af5
w1sp <semen2002bs@gmail.com>
2023-12-01 22:49:04 +0300

    log sensor data

--- app/src/main/java/com/example/bss_mobile_lab_4_5/MainActivity.kt
+++ app/src/main/java/com/example/bss_mobile_lab_4_5/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.bss_mobile_lab_4_5
 
+
+import android.R.attr.orientation
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.DisplayMetrics
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
@@ -28,19 +34,43 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     var mMaxValue = 0f
     var mValue = 0f
 
+    var compass: ImageView? = null
+    var boll: ImageView? = null
+    var test: ImageView? = null
+
+    var minX: Float = 0F
+    var maxX: Float = 0F
+    var minY: Float = 0F
+    var maxY: Float = 0F
+
+    private var lastAccelerometer: FloatArray = FloatArray(3)
+    private var lastMagnetometer: FloatArray = FloatArray(3)
+    private var lastAccelerometerSet: Boolean = false
+    private var lastMagnetometerSet: Boolean = false
+    private var rotationMatrix: FloatArray = FloatArray(9)
+    private var orientation: FloatArray = FloatArray(3)
+
     @SuppressLint("CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mAccelerometerX = findViewById<View>(R.id.textView) as TextView
-        mAccelerometerY = findViewById<View>(R.id.textView2) as TextView
-        mAccelerometerZ = findViewById<View>(R.id.textView3) as TextView
-        mMagneticX = findViewById<View>(R.id.textView5) as TextView
-        mMagneticY = findViewById<View>(R.id.textView6) as TextView
-        mMagneticZ = findViewById<View>(R.id.textView7) as TextView
-        mProximity = findViewById<View>(R.id.textView9) as TextView
-        mLight = findViewById<View>(R.id.textView11) as TextView
+
+        test = findViewById(R.id.imageView3)
+
+        minX = 50F
+        maxX = 200F
+        minY = 50F
+        maxY = 200F
+
+        mAccelerometerX = findViewById(R.id.textView)
+        mAccelerometerY = findViewById(R.id.textView2)
+        mAccelerometerZ = findViewById(R.id.textView3)
+        mMagneticX = findViewById(R.id.textView5)
+        mMagneticY = findViewById(R.id.textView6)
+        mMagneticZ = findViewById(R.id.textView7)
+        mProximity = findViewById(R.id.textView9)
+        mLight = findViewById(R.id.textView11)
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
         mAccelerometerSensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         mMagneticSensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
@@ -51,7 +81,65 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     @SuppressLint("SetTextI18n")
     override fun onSensorChanged(event: SensorEvent) {
+
+//        test!!.rotation += 10F
+
+        val outMetrics = DisplayMetrics()
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            val display = display
+            display?.getRealMetrics(outMetrics)
+        } else {
+            val display = windowManager.defaultDisplay
+            display.getMetrics(outMetrics)
+        }
+
+
+        if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, lastMagnetometer, 0, event.values.size)
+            lastMagnetometerSet = true
+        } else if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, lastAccelerometer, 0, event.values.size)
+            lastAccelerometerSet = true
+        }
+
+        if (lastAccelerometerSet && lastMagnetometerSet) {
+            SensorManager.getRotationMatrix(
+                rotationMatrix,
+                null,
+                lastAccelerometer,
+                lastMagnetometer
+            )
+            SensorManager.getOrientation(rotationMatrix, orientation)
+            val azimuthInRadians: Float = orientation[0]
+            val azimuthInDegrees =
+                (Math.toDegrees(azimuthInRadians.toDouble()) + 360).toFloat() % 360
+
+            test!!.rotation = azimuthInDegrees
+        }
+
+
         if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+
+            var xVal = (event.values[0] / 10F) * -1
+            var yVal = (event.values[1] / 10F) * 1
+
+            if (test!!.x + xVal <= minX) {
+                test!!.x = minX
+            } else if (test!!.x + xVal >= maxX) {
+                test!!.x = maxX
+            } else {
+                test!!.x += xVal
+            }
+
+            if (test!!.y + yVal <= minY) {
+                test!!.y = minY
+            } else if (test!!.y + yVal >= maxY) {
+                test!!.y = maxY
+            } else {
+                test!!.y += yVal
+            }
+
             mAccelerometerX!!.text = event.values[0].toString()
             mAccelerometerY!!.text = event.values[1].toString()
             mAccelerometerZ!!.text = event.values[2].toString()
